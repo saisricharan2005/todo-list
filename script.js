1f8575b5408733b8b13a7841c000f6b4d98959d7
Charan <saisricharan2005@gmail.com>
2025-07-27 17:35:19 +0530

    commit2

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-// let ele = document.createElement("p");
-// ele.textContent = "Hello, World!";
-// document.body.appendChild(ele);
+let todoItemContainer = document.getElementById("todoItemsContainer");
+
+let todoElement = document.createElement('li');
+todoElement.classList.add('todo-item-container', 'd-flex', 'flex-row');
+todoItemContainer.appendChild(todoElement);
+
+let inputElement = document.createElement('input');
+inputElement.type = "checkbox";
+inputElement.id = "checkboxInput"; 
+inputElement.classList.add('checkbox-input');
+todoElement.appendChild(inputElement);
+
+let labelContainer = document.createElement('div');
+labelContainer.classList.add('label-container', 'd-flex','flex-row');
+todoElement.appendChild(labelContainer);
+
+
