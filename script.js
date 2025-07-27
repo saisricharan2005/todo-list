0203bd0406a39a6337ffdea2a6cca389706293a4
Charan <saisricharan2005@gmail.com>
2025-07-28 01:09:47 +0530

    commit2

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-let todoItemContainer = document.getElementById("todoItemsContainer");
+let todoItemsContainer = document.getElementById("todoItemsContainer");
 
-let todoElement = document.createElement('li');
-todoElement.classList.add('todo-item-container', 'd-flex', 'flex-row');
-todoItemContainer.appendChild(todoElement);
+let todolist = [
+    {
+        text : "Learn HTML",
+    },
+    {
+        text : "Learn CSS",
+    },
+    {
+        text : "Learn JavaScript",
+    }
+];
 
-let inputElement = document.createElement('input');
-inputElement.type = "checkbox";
-inputElement.id = "checkboxInput"; 
-inputElement.classList.add('checkbox-input');
-todoElement.appendChild(inputElement);
+function createAndAppend(todo) {
+    let todoElement = document.createElement('li');
+    todoElement.classList.add('todo-item-container', 'd-flex', 'flex-row');
+    todoItemsContainer.appendChild(todoElement);
 
-let labelContainer = document.createElement('div');
-labelContainer.classList.add('label-container', 'd-flex','flex-row');
-todoElement.appendChild(labelContainer);
+    let inputElement = document.createElement('input');
+    inputElement.type = "checkbox";
+    inputElement.id = "checkboxInput"; 
+    inputElement.classList.add('checkbox-input');
+    todoElement.appendChild(inputElement);
 
+    let labelContainer = document.createElement('div');
+    labelContainer.classList.add('label-container', 'd-flex', 'flex-row');
+    todoElement.appendChild(labelContainer)
 
+    let labelElement = document.createElement('label');
+    labelElement.setAttribute('for', 'checkboxInput');
+    labelElement.classList.add('checkbox-label');
+    labelElement.textContent = todo.text;
+    labelContainer.appendChild(labelElement);
+
+    let deleteContainer = document.createElement('div');
+    deleteContainer.classList.add('delete-icon-container');
+    labelContainer.appendChild(deleteContainer);
+
+    let deleteIcon = document.createElement('i');
+    deleteIcon.classList.add('fa', 'fa-trash', 'delete-icon');
+    deleteContainer.appendChild(deleteIcon);
+}
+
+for (let todo of todolist) {
+    createAndAppend(todo);
+}
